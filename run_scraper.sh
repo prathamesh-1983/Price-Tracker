58f8f9b22ab69f9ae7e45808278fdc0d753667ca
prathamesh-desai <prathameshudaydesai12345@gmail.com>
2026-08-23 14:09:34 +0530

    run_scraper.sh

---
+++ run_scraper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run the official Bright Data production scraper
+npx -p @brightdata/cli bdata scraper run c_msz13hto1gr71k0m9n "https://webscraper.io/test-sites/e-commerce/allinone/phones/touch"
